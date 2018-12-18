b1e8c2e56ab84df9a997cf8d6de57f66b419a51e
sameer-ahmed <me@sameerahmed.in>
2018-12-18 18:16:17 +0530

    fixed import issue

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import { NativeModules } from 'react-native';
 
 const { RNAndroidSmsRetriever } = NativeModules;
 
-export default {
-    startSmsRetriever: (successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetriever(successCallback, failureCallback),
-    startSmsRetrieverForOtp: (successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetrieverForOtp(successCallback, failureCallback),
-    startSmsRetrieverForOtpLength: (otpLength, successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetrieverForOtpLength(otpLength, successCallback, failureCallback),
-    startSmsRetrieverForRegex: (regex, successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetrieverForRegex(regex, successCallback, failureCallback),
-};
+export const startSmsRetriever = (successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetriever(successCallback, failureCallback)
+
+export const startSmsRetrieverForOtp = (successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetrieverForOtp(successCallback, failureCallback)
+
+export const startSmsRetrieverForOtpLength = (otpLength, successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetrieverForOtpLength(otpLength, successCallback, failureCallback)
+
+export const startSmsRetrieverForRegex = (regex, successCallback, failureCallback) => RNAndroidSmsRetriever.startSmsRetrieverForRegex(regex, successCallback, failureCallback)
